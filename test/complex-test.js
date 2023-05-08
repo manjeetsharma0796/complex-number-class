e538ecea464dd996d391b949ab9ed1e872a3404a
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-08 19:04:25 +0530

    Add add method to Complex, functional to add two complex number

--- test/complex-test.js
+++ test/complex-test.js
@@ -4,9 +4,19 @@ const {Complex} = require('../src/complex.js');
 
 describe('complex', function() {
   describe('toString', function() {
-    const a = new Complex(1, 2);
     it('Should display complex number', function() {
-      deepStrictEqual(a.toString(), '1 + 2i')
+      const num = new Complex(1, 2);
+      deepStrictEqual(num.toString(), '1 + 2i');
     });
   });
+
+  describe('add', function() {
+    it('Should add two complex number', function() {
+      const num1 = new Complex(1, 2);
+      const num2 = new Complex(2, 3);
+      const result = num1.add(num2);
+
+      deepStrictEqual(result.toString(), '3 + 5i');
+    })
+  })
 });
