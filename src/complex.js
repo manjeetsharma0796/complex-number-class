f9743a8e1da5b8e716ff7a962bae8f61b585276b
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-08 18:48:41 +0530

    Git initial commit

---
+++ src/complex.js
@@ -0,0 +1,22 @@
+class Complex {
+  #real
+  #imaginary
+
+  constructor (real, imaginary) {
+    this.#real = real;
+    this.#imaginary = imaginary;
+  };
+
+  add(other) {
+    const real = this.#real + other.#real;
+    const imaginary = this.#imaginary + other.#imaginary;
+
+    return new Complex(real, imaginary);
+  };
+
+  toString() {
+    return this.#real + " + " + this.#imaginary;
+  };
+};
+
+exports.Complex = Complex;
---
+++ test/complex-test.js
@@ -0,0 +1,12 @@
+const {describe, it} = require('node:test');
+const {deepStrictEqual} = require('assert');
+const {Complex} = require('../src/complex.js');
+
+describe('complex', function() {
+  describe('toString', function() {
+    const a = new Complex(1, 2);
+    it('Should display complex number', function() {
+      deepStrictEqual(a.toString(), '1 + 2')
+    });
+  });
+});
