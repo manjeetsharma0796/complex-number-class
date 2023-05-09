343fd350bc215d06471174ccabe8add84565ed2e
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-05-09 11:15:40 +0530

    Refactored class Complex

--- src/complex.js
+++ src/complex.js
@@ -7,40 +7,40 @@ class Complex {
   #imaginary
 
   constructor(real, imaginary) {
-    this.#real = real.value();
-    this.#imaginary = imaginary.value();
+    this.#real = real;
+    this.#imaginary = imaginary;
   };
 
   add(other) {
-    const real = new Real(this.#real + other.#real);
-    const imaginary = new Imaginary(this.#imaginary + other.#imaginary);
+    const real = new Real(this.#real.value() + other.#real.value());
+    const imaginary = new Imaginary(this.#imaginary.value() + other.#imaginary.value());
 
     return new Complex(real, imaginary);
   };
 
   subtract(other) {
-    const real = new Real(this.#real - other.#real);
-    const imaginary = new Imaginary(this.#imaginary - other.#imaginary);
+    const real = new Real(this.#real.value() - other.#real.value());
+    const imaginary = new Imaginary(this.#imaginary.value() - other.#imaginary.value());
 
     return new Complex(real, imaginary);
   };
 
   multiply(other) {
-    const real = new Real((this.#real * other.#real) - (this.#imaginary * other.#imaginary));
-    const imaginary = new Imaginary((this.#real * other.#imaginary) + (this.#imaginary * other.#real));
+    const real = new Real((this.#real.value() * other.#real.value()) - (this.#imaginary.value() * other.#imaginary.value()));
+    const imaginary = new Imaginary((this.#real.value() * other.#imaginary.value()) + (this.#imaginary.value() * other.#real.value()));
 
     return new Complex(real, imaginary);
   }
 
   #decideSymbol() {
-    return this.#imaginary < 0 ? '-' : '+';
+    return this.#imaginary.value() < 0 ? '-' : '+';
   }
 
   toString() {
-    const imaginaryAbsValue = Math.abs(this.#imaginary);
+    const imaginaryAbsValue = Math.abs(this.#imaginary.value());
     const symbol = this.#decideSymbol();
 
-    return `${this.#real} ${symbol} ${imaginaryAbsValue}i`;
+    return `${this.#real.value()} ${symbol} ${imaginaryAbsValue}i`;
   };
 };
 
